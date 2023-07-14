8448e1e99de58612b8504b68ea3ac20163bc18f8
specht <ole.specht@posteo.de>
2023-07-14 16:39:07 +0200

    glow

--- install.sh
+++ install.sh
@@ -6,5 +6,11 @@ sudo apt update
 sudo apt install fish neovim universal-ctags
 chsh -s /usr/bin/fish
 cp .config ~/.config
+# omf
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf update
+# glow
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
