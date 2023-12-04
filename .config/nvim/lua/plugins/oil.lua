d1a53a46da4ce30191fb61e2c2468c9f3b106dff
Ole Specht <ole.specht@ifam.fraunhofer.de>
2023-12-04 17:02:56 +0100

    floating window

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -4,13 +4,28 @@ return {
             'stevearc/oil.nvim',
             dependencies = { "nvim-tree/nvim-web-devicons" },
             keys = {
-                {"<F3>", "<cmd>30vsplit | Oil<cr>", desc="file explorer"},
+                {"<F3>", "<cmd>lua require('oil').toggle_float()<cr>", desc="file explorer"},
             },
             opts = {
                 use_default_keymaps = true,
                 view_options = {
                     show_hidden = true,
                 },
+                float = {
+                    -- Padding around the floating window
+                    padding = 10,
+                    max_width = 40,
+                    max_height = 0,
+                    border = "rounded",
+                    win_options = {
+                        winblend = 0,
+                    },
+                    -- This is the config that will be passed to nvim_open_win.
+                    -- Change values here to customize the layout
+                    override = function(conf)
+                        return conf
+                    end,
+              },
             },
         }
 }
