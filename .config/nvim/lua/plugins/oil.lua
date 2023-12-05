e9747e339bed8e9b7684c59a1b79256d59a3d6e8
Ole Specht <ole.specht@ifam.fraunhofer.de>
2023-12-05 11:01:23 +0100

    fixed oil config

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,31 +1,30 @@
 return {
     -- file explorer as buffer
-        {
-            'stevearc/oil.nvim',
-            dependencies = { "nvim-tree/nvim-web-devicons" },
-            keys = {
-                {"<F3>", "<cmd>lua require('oil').toggle_float()<cr>", desc="file explorer"},
+    {
+        'stevearc/oil.nvim',
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            default_file_explorer = true,
+            use_default_keymaps = true,
+            view_options = {
+                show_hidden = true,
             },
-            opts = {
-                use_default_keymaps = true,
-                view_options = {
-                    show_hidden = true,
+            float = {
+                -- Padding around the floating window
+                padding = 10,
+                max_width = 40,
+                max_height = 0,
+                border = "rounded",
+                win_options = {
+                    winblend = 0,
                 },
-                float = {
-                    -- Padding around the floating window
-                    padding = 10,
-                    max_width = 40,
-                    max_height = 0,
-                    border = "rounded",
-                    win_options = {
-                        winblend = 0,
-                    },
-                    -- This is the config that will be passed to nvim_open_win.
-                    -- Change values here to customize the layout
-                    override = function(conf)
-                        return conf
-                    end,
-              },
+                -- This is the config that will be passed to nvim_open_win.
+                -- Change values here to customize the layout
+                override = function(conf)
+                    return conf
+                end,
             },
-        }
+            vim.keymap.set("n", "<F3>", "<cmd>lua require('oil').toggle_float()<cr>", { desc="file explorer" })
+        },
+    },
 }
