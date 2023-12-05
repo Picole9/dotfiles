bde31c7b4d98707a8be7d9dfa19cd41cab64cc28
Ole Specht <ole.specht@ifam.fraunhofer.de>
2023-12-05 10:58:27 +0100

    added aliases for shell-nav, docker, cleaned repo

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,24 +2,42 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias lse='exa -lga --icons'
+alias l='exa -lga --icons'
 alias bat='batcat'
+alias ..='cd ..'
+alias ...='cd ../..'
 
+# vi
 alias vi='nvim'
+function vissh
+    nvim oil-ssh://$argv
+end
 
+# docker
+alias dc='docker compose'
+alias dcl='docker compose ps'
+alias dcd='docker compose down'
+alias dcu='docker compose up -d --remove-orphans'
+alias dprune='docker system prune -a'
+alias dlog='docker compose logs -f'
+
+# git
 alias gc='git commit -m'
 alias ga='git add'
+alias gs='git status'
+alias gp='git push origin master'
 
+# python
 alias python='python3'
 alias py='python3'
 
 set -gx EDITOR nvim
-set -gx PROJECT_PATHS ~/git
+set -gx PROJECT_PATHS ~/git ~/.config/
 
 function fish_greeting
     echo \((date +%T)\) Moin $USER auf $hostname
 end
 
-if test -e ~/.config/fish/local_config.fish
-    source ~/.config/fish/local_config.fish
+if test -e ~/.config/fish/localconf.fish
+    source ~/.config/fish/localconf.fish
 end
