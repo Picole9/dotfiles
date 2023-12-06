06027adbee7ea057f96bb30e3a3290db80dc649b
Ole Specht <ole.specht@ifam.fraunhofer.de>
2023-12-06 16:47:48 +0100

    bufferline

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,22 +5,6 @@ return {
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         opts = {
             icons_enabled = true,
-            tabline = {
-                lualine_a = {'buffers'},
-                lualine_b = {},
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {'datetime'}
-            },
-            winbar = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {}
-            },
-        }
+        },
     }
 }
