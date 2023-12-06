7d9f633aec88e7517948738c9c8cc8b26da32028
Ole Specht <ole.specht@ifam.fraunhofer.de>
2023-12-06 16:47:23 +0100

    cleaned repo

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -26,4 +26,11 @@ return {
             },
         },
     },
+    -- comment-highlight for todo, hack, warn, parf, note, test, fix
+    {
+        "folke/todo-comments.nvim",
+        dependencies = {"nvim-lua/plenary.nvim"},
+        opts = {}
+    },
+
 }
