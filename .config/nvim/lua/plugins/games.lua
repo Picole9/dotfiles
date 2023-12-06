eb03c8addca5880225753f49600869f951ad0ce0
Ole Specht <ole.specht@ifam.fraunhofer.de>
2023-12-06 16:46:02 +0100

    games

---
+++ .config/nvim/lua/plugins/games.lua
@@ -0,0 +1,12 @@
+return {
+    -- tetris
+    {
+        "alec-gibson/nvim-tetris",
+        enabled = false,
+    },
+    -- minesweeper
+    {
+        "seandewar/nvimesweeper",
+        enabled = false,
+    }
+}
