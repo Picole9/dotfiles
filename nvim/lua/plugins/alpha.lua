9ada89138bbc27bd49ae02e615d70ea2cbcda8c0
specht <ole.specht@posteo.de>
2023-12-18 11:51:59 +0100

    updated icons to nerdfont v3

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -21,12 +21,12 @@ return {
 
             -- Set menu
             dashboard.section.buttons.val = {
-                dashboard.button( "n", "  > New file" , ":ene <BAR> startinsert <CR>"),
+                dashboard.button( "n", "  > New file" , ":ene <BAR> startinsert <CR>"),
                 dashboard.button( "e", "  > Explorer" , ":Oil <CR>"),
-                dashboard.button( "f", "  > Find file", ":Telescope find_files<CR>"),
+                dashboard.button( "f", "󰱽  > Find file", ":Telescope find_files<CR>"),
                 dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
                 dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-                dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+                dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
             }
             alpha.setup(dashboard.opts)
 
