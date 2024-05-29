391e0b069ce2ad8e7eadad21edef8e260f5afad3
Ole Specht <ole.specht@ifam.fraunhofer.de>
2024-05-29 11:14:21 +0200

    [nvim] treesitter added markdown_inline

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
                 "json",
                 "lua",
                 "markdown",
+                "markdown_inline",
                 "python",
                 "query",
                 "vim",
