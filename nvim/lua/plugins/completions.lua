83c6a5ea70b83673a4772c9f4cf4d59115c2703c
Ole Specht <ole.specht@ifam.fraunhofer.de>
2024-05-29 11:13:03 +0200

    [nvim] completions added symbols + buffer -and pathsource

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -4,13 +4,17 @@ return {
         'hrsh7th/nvim-cmp',
         dependencies = {
             'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
             'L3MON4D3/LuaSnip',
             'saadparwaiz1/cmp_luasnip',
             'rafamadriz/friendly-snippets',
+            'onsails/lspkind.nvim',
         },
         config = function()
             local cmp = require("cmp")
             local luasnip = require("luasnip")
+            local lspkind = require("lspkind")
             luasnip.config.setup {}
             require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -54,9 +58,16 @@ return {
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
                     { name = 'luasnip' },
-                }, {
                     { name = 'buffer' },
+                    { name = 'path' },
                 }),
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = "symbol_text",
+                        maxwidth = 50,
+                        ellipsis_car = "...",
+                    }),
+                },
             })
         end,
     }
