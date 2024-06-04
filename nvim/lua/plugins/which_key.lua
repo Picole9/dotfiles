183f9dedfd0dccde576c88be833a8054cb7489e6
Ole Specht <ole.specht@ifam.fraunhofer.de>
2024-06-04 11:00:49 +0200

    [nvim] easier which-keys

--- nvim/lua/plugins/which_key.lua
+++ nvim/lua/plugins/which_key.lua
@@ -1,20 +1,20 @@
 return {
-    -- popup for keybindings
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {},
-        config = function ()
-            require('which-key').register {
-                ['<leader>c'] = { name = '[C]omment', _ = 'which_key_ignore' },
-                ['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
-                ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-                ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-            }
-        end,
-    }
+	-- popup for keybindings
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			plugins = { spelling = true },
+			defaults = {},
+		},
+		config = function(_, opts)
+			local wk = require("which-key")
+			wk.setup(opts)
+			wk.register(opts.defaults)
+		end,
+	},
 }
