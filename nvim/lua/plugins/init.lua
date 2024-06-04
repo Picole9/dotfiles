bd2025c3c4f5cd3199eb054a500264bd72bbb3ef
Ole Specht <ole.specht@ifam.fraunhofer.de>
2024-06-04 10:58:57 +0200

    [nvim] init-plugins into seperate files

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,17 +1,1 @@
-return {
-    { -- view markdown, requires glow as package
-        "ellisonleao/glow.nvim",
-        opts = {},
-        cmd = "Glow"
-    },
-    { -- indent lines
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {}
-    },
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {},
-    },
-}
+return {}
