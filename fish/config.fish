13f08f299839dab36e8af0b374d2b9bbc0499ca5
Ole Specht <ole.specht@ifam.fraunhofer.de>
2024-06-12 11:25:52 +0200

    [fish] various python-venv functions

--- fish/config.fish
+++ fish/config.fish
@@ -13,7 +13,9 @@ alias bat='bat --paging=never'
 alias t='tmux'
 
 # expands !! to the last history item
-function last_history_item; echo $history[1]; end
+function last_history_item
+    echo $history[1]
+end
 abbr -a !! --position anywhere --function last_history_item
 
 # vi
@@ -44,6 +46,41 @@ end
 # python
 alias python='python3'
 alias py='python3'
+
+function pyenv
+    python3 -m venv .venv
+    source .venv/bin/activate.fish
+    if test -e "requirements.txt"
+        pip install -r requirements.txt
+    end
+end
+
+function pyrun -d "run python script in venv"
+    if test -n "$VIRTUAL_ENV"
+        deactivate
+    end
+    set pydir (dirname $argv[1])
+    if test -e "$pydir/.venv/"
+        source $pydir/.venv/bin/activate.fish
+        python3 $argv
+        deactivate
+    else
+        echo "no virtual_env found"
+    end
+end
+
+function auto_activate_venv --on-variable PWD -d "auto-activate venv if exists on change-directory"
+    set REPO_ROOT (git rev-parse --show-toplevel 2>/dev/null)
+    if test -z "$REPO_ROOT"; and test -n "$VIRTUAL_ENV"
+        deactivate
+    end
+    if [ "$VIRTUAL_ENV" = "$REPO_ROOT/.venv" ]
+        return
+    end
+    if [ -d "$REPO_ROOT/.venv" ]
+        source "$REPO_ROOT/.venv/bin/activate.fish" &>/dev/null
+    end
+end
 
 alias wetter='curl https://wttr.in/Bremen'
 
