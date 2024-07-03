1fcc53432c6d003db08a69acbacab13d74c7c998
Ole Specht <ole.specht@ifam.fraunhofer.de>
2024-07-03 10:38:58 +0200

    [nvim] oil preview

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,30 +1,26 @@
 return {
-    -- file explorer as buffer
-    {
-        'stevearc/oil.nvim',
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {
-            default_file_explorer = true,
-            use_default_keymaps = true,
-            view_options = {
-                show_hidden = true,
-            },
-            float = {
-                -- Padding around the floating window
-                padding = 10,
-                max_width = 40,
-                max_height = 0,
-                border = "rounded",
-                win_options = {
-                    winblend = 0,
-                },
-                -- This is the config that will be passed to nvim_open_win.
-                -- Change values here to customize the layout
-                override = function(conf)
-                    return conf
-                end,
-            },
-            vim.keymap.set("n", "<leader><F3>", "<cmd>lua require('oil').toggle_float()<cr>", { desc="file explorer" })
-        },
-    },
+	-- file explorer as buffer
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			default_file_explorer = true,
+			use_default_keymaps = true,
+			view_options = {
+				show_hidden = true,
+			},
+			float = {
+				padding = 2,
+				max_width = 80,
+				max_height = 40,
+				preview_split = "right",
+			},
+			vim.keymap.set(
+				"n",
+				"<leader><F3>",
+				"<cmd>lua require('oil').toggle_float()<cr>",
+				{ desc = "file explorer" }
+			),
+		},
+	},
 }
