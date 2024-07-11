b072414288cf1f8435cab3a7db92f887efce6f4f
Ole Specht <ole.specht@ifam.fraunhofer.de>
2024-07-11 16:06:57 +0200

    [fish] added get_cert vi-bindings sudo

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,7 @@ alias l='eza -lga --icons'
 if test (cat /etc/os-release | grep -i debian)
     alias bat='batcat --paging=never'
     alias bathelp='batcat --plain --language=help'
-else    
+else
     alias bat='bat --paging=never'
     alias bathelp='bat --plain --language=help'
 end
@@ -94,6 +94,12 @@ function auto_activate_venv --on-variable PWD -d "auto-activate venv if exists o
     end
 end
 
+# vi mode
+function fish_user_key_bindings
+    fish_vi_key_bindings
+    bind -M insert -m default jk backward-char force-repaint
+end
+
 alias wetter='curl https://wttr.in/Bremen'
 
 set -gx EDITOR nvim
@@ -107,4 +113,50 @@ end
 
 if test -e ~/.config/fish/localconf.fish
     source ~/.config/fish/localconf.fish
+end
+
+function get_cert -d "get x509 cert from url; get_cert {domain} [port:443]"
+    set domain $argv[1]
+    if test (count $argv) -gt 1
+        set port "$argv[2]"
+    else
+        set port 443
+    end
+    set -l cert_file (echo | openssl s_client -connect $domain:$port 2>/dev/null | openssl x509)
+    if test -n "$cert_file"
+        echo "$cert_file"
+    else
+        echo "Failed to fetch the certificate."
+        return 1
+    end
+    while read --nchars 1 -l save_cert -P 'Do you want to save the certificate to /etc/ssl/ca-cert? (y/n)' or return 1
+        switch $save_cert
+            case y Y
+                if not test -d /etc/ssl/ca-cert
+                    sudo mkdir -p /etc/ssl/ca-cert
+                end
+                echo | openssl s_client -connect $domain:$port 2>/dev/null | openssl x509 >/etc/ssl/ca-cert/$domain.crt
+                cat /etc/ssl/ca-cert/$domain.crt | sudo tee -a /etc/ssl/certs/ca-certificates.crt >/dev/null
+                if test $status -eq 0
+                    echo "Certificate saved to /etc/ssl/ca-cert/$domain.crt"
+                else
+                    echo "Failed to save the certificate. Please check your permissions."
+                    return 1
+                end
+                break
+            case n N
+                echo "Certificate not saved."
+                break
+            case '*'
+                echo no valid input
+                continue
+        end
+    end
+end
+
+function sudo
+    if functions -q $argv[1]
+        set argv fish -c "$argv"
+    end
+    command sudo $argv
 end
