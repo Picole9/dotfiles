f3dbc9c5f5f8adbd35f19bb2869e1b9e069ba0aa
Ole Specht <ole.specht@ifam.fraunhofer.de>
2024-08-06 09:03:18 +0200

    moved functions to fisher-plugins

--- fish/config.fish
+++ fish/config.fish
@@ -16,7 +16,7 @@ else
     alias bat='bat --paging=never'
     alias bathelp='bat --plain --language=help'
 end
-alias t='tmux'
+alias py='python3'
 
 # expands !! to the last history item
 function last_history_item
@@ -47,50 +47,13 @@ alias ga='git add'
 alias gs='git status'
 alias gdt='git difftool'
 alias gpull='git pull --rebase'
+alias gp='git push'
 alias gpush='git push'
 function gd
     if test (cat /etc/os-release | grep -i debian)
         git diff --name-only --relative --diff-filter=d $argv | xargs batcat --diff
     else
         git diff --name-only --relative --diff-filter=d $argv | xargs bat --diff
-    end
-end
-
-# python
-alias py='python'
-
-function pyenv -d "create venv"
-    python3 -m venv .venv
-    source .venv/bin/activate.fish
-    if test -e "requirements.txt"
-        pip install -r requirements.txt
-    end
-end
-
-function pyrun -d "run python script in venv"
-    if test -n "$VIRTUAL_ENV"
-        deactivate
-    end
-    set pydir (dirname $argv[1])
-    if test -e "$pydir/.venv/"
-        source $pydir/.venv/bin/activate.fish
-        python3 $argv
-        deactivate
-    else
-        echo "no virtual_env found"
-    end
-end
-
-function auto_activate_venv --on-variable PWD -d "auto-activate venv if exists on change-directory"
-    set REPO_ROOT (git rev-parse --show-toplevel 2>/dev/null)
-    if test -z "$REPO_ROOT"; and test -n "$VIRTUAL_ENV"
-        deactivate
-    end
-    if [ "$VIRTUAL_ENV" = "$REPO_ROOT/.venv" ]
-        return
-    end
-    if [ -d "$REPO_ROOT/.venv" ]
-        source "$REPO_ROOT/.venv/bin/activate.fish" &>/dev/null
     end
 end
 
