9824ea522b214902ddaa8cd6030db8ca7042ae63
Ole Specht <ole.specht@ifam.fraunhofer.de>
2024-09-04 12:43:55 +0200

    [nvim] statusline, yaml-formatter

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,31 @@
 return {
-    -- statusbar
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        opts = {
-            icons_enabled = true,
-        },
-    }
+	-- statusbar
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"stevearc/conform.nvim",
+		},
+		opts = {
+			icons_enabled = true,
+			sections = {
+				lualine_c = {
+					function()
+						local clients
+						clients = vim.lsp.get_active_clients()
+						local clients_list = {}
+						for _, client in pairs(clients) do
+							table.insert(clients_list, client.name)
+						end
+						for _, formatter in pairs(require("conform").formatters_by_ft[vim.bo.filetype]) do
+							table.insert(clients_list, formatter)
+						end
+						if #clients_list ~= 0 then
+							return "󱉶 " .. table.concat(clients_list, ", ")
+						end
+					end,
+				},
+			},
+		},
+	},
 }
